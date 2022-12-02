2fa1cd77a1d316c8f7e0144d8705936eb10f07dd
Angelo Carvalho <angelosuporteti@gmail.com>
2022-12-02 18:40:22 -0300

    Tratamento de excecoes - Bloco try e catch

---
+++ java-tratamento-excecao-try-catch/src/programa.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+import java.util.InputMismatchException;
+
+
+public class programa {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+	try {
+		String[] vet = sc.nextLine().split(" ");
+		int posicao = sc.nextInt();
+		System.out.println(vet[posicao]);
+
+	}
+	catch(ArrayIndexOutOfBoundsException e) {
+		System.out.println("Posicao invalida");
+	}
+	catch(InputMismatchException e) {
+		System.out.println("Voce digitou um dado invalido. Digite apenas numeros inteiros");
+	}
+	System.out.println("Fim do programa");
+	sc.close();
+	}
+
+}
